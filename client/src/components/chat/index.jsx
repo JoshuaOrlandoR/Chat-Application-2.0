c5f48f077f1e6bda7183bdbe995934891fbc813e
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2023-04-27 16:26:40 -0400

    Started front-end work on new chat-project

---
+++ client/src/components/chat/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-engine-advanced';
+import Header from "@/components/customHeader";
+
+const Chat = () => {
+    const chatProps = useMultiChatLogic(
+        import.meta.env.VITE_PROJECT_ID,
+        "testuser1234",
+        "1234"
+    )
+
+  return (
+    <div style={{ flexBasis: "100%" }}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow 
+            {...chatProps}
+            style={{ height: "100vh" }}
+            renderChatHeader={(chat) => <Header chat={chat} />}
+        />
+    </div>
+  )
+}
+
+export default Chat
